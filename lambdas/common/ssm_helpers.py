06d16cb4fb2560e621543a3556b88ee96cbbb5cb
domjgiordano <dominick.giordano@payzer.com>
2024-10-09 18:12:45 -0400

    learn how 2 spell

--- lambdas/common/ssm_helpers.py
+++ lambdas/common/ssm_helpers.py
@@ -8,12 +8,12 @@ __SPOTIFY_ROOT = f'/{PRODUCT}/spotify/'
 __API_ROOT = f'/{PRODUCT}/api/'
 
 # AWS
-AWS_ACCESS_KEY = ssm.get_parameter(Name=f'{__AWS_ROOT}ACCESS_KEY', WithDecryption=True)['Paramter']['Value']
-AWS_SECRET_KEY = ssm.get_parameter(Name=f'{__AWS_ROOT}SECRET_KEY', WithDecryption=True)['Paramter']['Value']
+AWS_ACCESS_KEY = ssm.get_parameter(Name=f'{__AWS_ROOT}ACCESS_KEY', WithDecryption=True)['Parameter']['Value']
+AWS_SECRET_KEY = ssm.get_parameter(Name=f'{__AWS_ROOT}SECRET_KEY', WithDecryption=True)['Parameter']['Value']
 
 # SPOTIFY
-SPOTIFY_CLIENT_ID = ssm.get_parameter(Name=f'{__SPOTIFY_ROOT}CLIENT_ID', WithDecryption=True)['Paramter']['Value']
-SPOTIFY_CLIENT_SECRET = ssm.get_parameter(Name=f'{__SPOTIFY_ROOT}CLIENT_SECRET', WithDecryption=True)['Paramter']['Value']
+SPOTIFY_CLIENT_ID = ssm.get_parameter(Name=f'{__SPOTIFY_ROOT}CLIENT_ID', WithDecryption=True)['Parameter']['Value']
+SPOTIFY_CLIENT_SECRET = ssm.get_parameter(Name=f'{__SPOTIFY_ROOT}CLIENT_SECRET', WithDecryption=True)['Parameter']['Value']
 
 #API
-API_SECRET_KEY = ssm.get_parameter(Name=f'{__API_ROOT}API_SECRET_KEY', WithDecryption=True)['Paramter']['Value']
+API_SECRET_KEY = ssm.get_parameter(Name=f'{__API_ROOT}API_SECRET_KEY', WithDecryption=True)['Parameter']['Value']
