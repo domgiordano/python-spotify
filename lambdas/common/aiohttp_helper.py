e08945d7a5328c1fc455e802266b980772b3b774
Dominick Giordano <dominickgiordano@Dominicks-MacBook-Pro.local>
2025-09-12 14:29:51 -0400

    Add some extra waiting

--- lambdas/common/aiohttp_helper.py
+++ lambdas/common/aiohttp_helper.py
@@ -4,13 +4,24 @@ from lambdas.common.constants import LOGGER
 
 log = LOGGER.get_logger(__file__)
 
+
+# Global rate limit event
+rate_limited = asyncio.Event()
+rate_limited.set()  # start in "open" state
+
 async def fetch_json(session: aiohttp.ClientSession, url: str, headers: dict = None):
     try:
+        await rate_limited.wait()  # wait if rate limited globally
         async with session.get(url, headers=headers) as resp:
             if resp.status == 429:
                 retry_after = int(resp.headers.get('Retry-After', 1))
                 log.warning(f"Rate limit reached for GET {url}. Retrying after {retry_after} seconds...")
+
+                # Block everyone - wait - reset
+                rate_limited.clear()
                 await asyncio.sleep(retry_after + 1)
+                rate_limited.set()
+                # Retry
                 return await fetch_json(session, url, headers)
 
             if resp.status != 200:
@@ -25,11 +36,16 @@ async def fetch_json(session: aiohttp.ClientSession, url: str, headers: dict = N
 
 async def post_json(session: aiohttp.ClientSession, url: str, headers: dict = None, json: dict = None):
     try:
+        await rate_limited.wait()  # wait if rate limited globally
         async with session.post(url, headers=headers, json=json) as resp:
             if resp.status == 429:
                 retry_after = int(resp.headers.get('Retry-After', 1))
                 log.warning(f"Rate limit reached for POST {url}. Retrying after {retry_after} seconds...")
+                # Block everyone - wait - reset
+                rate_limited.clear()
                 await asyncio.sleep(retry_after + 1)
+                rate_limited.set()
+                # Retry
                 return await post_json(session, url, headers, json)
 
             if resp.status != 200 and resp.status != 201:
